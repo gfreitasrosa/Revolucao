0bada6c3e8d5717a8639f250c79cf2a388ea1eb0
Gabriel de Freitas Rosa <gabriel.rosa@benfatto.net.br>
2022-11-04 19:08:14 -0300

    Continuação da view

--- revolucao/webapp/controller/Main.controller.js
+++ revolucao/webapp/controller/Main.controller.js
@@ -1,17 +1,29 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, JSONModel) {
         "use strict";
 
         return Controller.extend("revolucao.controller.Main", {
             onInit: function () {
+                var ImageList = {
+                    Images: [
+                        {
+                            title: "Google",
+                            url: "https://www.google.com"
+                        }
+                        
 
+                    ]
+                };
+
+                var ImageModel = new JSONModel(ImageList);
+                this.getView().setModel(ImageModel, "ImageModel");
             },
-
             onPressBuscar: function(){
                 alert('Começou a revolução do SAP Fiori!');
             }
