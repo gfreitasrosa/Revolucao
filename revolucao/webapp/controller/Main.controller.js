783b7ecf91f7b3b06bf2a4d5fdeffde8ac486a05
Gabriel de Freitas Rosa <gabriel.rosa@benfatto.net.br>
2022-11-06 11:56:34 -0300

    Conexão com API e finalização do projeto

--- revolucao/webapp/controller/Main.controller.js
+++ revolucao/webapp/controller/Main.controller.js
@@ -25,7 +25,66 @@ sap.ui.define([
                 this.getView().setModel(ImageModel, "ImageModel");
             },
             onPressBuscar: function(){
-                alert('Começou a revolução do SAP Fiori!');
+                //alert('Começou a revolução do SAP Fiori!');
+                var oInputBusca = this.byId("inpBusca");
+                var sQuery = oInputBusca.getValue();
+
+                $.ajax({
+                    // cabeçalho    
+
+                    url: "https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI",
+                    method: "GET",
+                    async: true,
+                    corssDomain: true,
+                    jsonpCallback: "getJSON",
+                    contentType: "application/json",
+                    headers: {
+                        "X-RapidAPI-Key": "87f7709277mshb299561ee3d93d8p1452aajsnfa0505187d36",
+		                "X-RapidAPI-Host": "contextualwebsearch-websearch-v1.p.rapidapi.com"
+                    },
+
+                    //corpo
+
+                    data: {
+                        "q" : sQuery,
+                        "pageNumber" : 1,
+                        "pageSize" : 30,
+                        "autoCorrect" : true,
+                        "safeSearch" : true
+                    },
+
+                    //retorno em caso de sucesso
+
+                    success: function(data, textStatus){
+                        var oImageModel = this.getView().getModel("ImageModel");
+                        var oDadosImage = oImageModel.getData();
+
+
+                        oDadosImage = {
+                            Images: []
+                        };
+
+                        oImageModel.setData(oDadosImage);
+                        debugger 
+
+                        var listaResultados = data.value;
+                        var newItem;
+
+                        for(var i = 0; i < listaResultados.length; i++){
+                            newItem = listaResultados[i];
+                            oDadosImage.Images.push(newItem);
+                        };
+
+                        oImageModel.refresh();
+                    }.bind(this),
+
+                    //retorno em caso de erro
+
+                    error: function(){
+
+                    }.bind(this)
+
+                });//fim $.ajax({})
             }
         });
     });
